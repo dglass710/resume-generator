0c6154f0e8106d84a054a0a10ccc504db7cce916
David Glass <dglass2525@gmail.com>
2025-02-14 18:25:28 -0600

    Now storing role specific json files in main branch and added a bash script to quickly copy them to the application directory.

---
+++ update_local_json_files.sh
@@ -0,0 +1,3 @@
+cp role_specific_data/it_systems_administrator.json ~/Documents/ResumeGeneratorApp/ && echo Copied it_systems_administrator.json to the application folder
+cp role_specific_data/security_engineer.json ~/Documents/ResumeGeneratorApp/ && echo Copied security_engineer.json to the application folder
+cp role_specific_data/cybersecurity_analyst.json ~/Documents/ResumeGeneratorApp/ && echo Copied cybersecurity_analyst.json to the application folder
