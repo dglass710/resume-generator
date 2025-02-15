c58888153f2467f5ee763143e29f6bc455af8a8f
David Glass <dglass2525@gmail.com>
2025-02-14 21:22:17 -0600

    The new bash script copies the master json file too now.

--- update_local_json_files.sh
+++ update_local_json_files.sh
@@ -1,3 +1,4 @@
 cp role_specific_data/it_systems_administrator.json ~/Documents/ResumeGeneratorApp/ && echo Copied it_systems_administrator.json to the application folder
 cp role_specific_data/security_engineer.json ~/Documents/ResumeGeneratorApp/ && echo Copied security_engineer.json to the application folder
 cp role_specific_data/cybersecurity_analyst.json ~/Documents/ResumeGeneratorApp/ && echo Copied cybersecurity_analyst.json to the application folder
+cp role_specific_data/master.json ~/Documents/ResumeGeneratorApp/ && echo Copied master.json to the application folder
