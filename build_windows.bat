e7cdf3867e491f24066bfd21665bf08ac905a4d4
David Glass <dag@Davids-MacBook-Pro.local>
2025-02-28 23:43:07 -0600

    Removed pause in build_windows.bat

--- build_windows.bat
+++ build_windows.bat
@@ -7,3 +7,5 @@ REM Build the executable with PyInstaller.
 REM The --noconfirm flag tells PyInstaller to overwrite existing builds without asking.
 pyinstaller --onefile --noconsole --noconfirm --add-data "data.json;." --add-data "default_data.json;." --add-data "generator.py;." ResumeBuilder.py
 
+REM Clean up the temporary VBScript.
+del sendkey.vbs
